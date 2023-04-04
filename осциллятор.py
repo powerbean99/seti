76bd37c15b7d040f030a2f8e3d2b83ff6fe432a3
powerbean99 <suguro100@gmail.com>
2023-04-04 13:16:03 +0500

    Update осциллятор.py

--- осциллятор.py
+++ осциллятор.py
@@ -11,14 +11,21 @@ def n(k):
     u, d = 0, 0.05
     noise = np.random.normal(u, d, size=length)
     return noise
-
 k = np.arange(0, 5*np.pi, 0.1)
 S = s(k)
 D = S + n(k)
 F = D +  a
-plt.plot(k, s(k), color='blue', label="s(k)")
-plt.plot(k, s(k)+n(k), label='s(k)+n(k)')
-plt.plot(k, s(k)+n(k)+a, label='s(k)+n(k)+a')
+def signaltoo(a, axis=0, ddof=0):
+    a = np.asanyarray(a)
+    m = a.mean(axis)
+    sd = a.std(axis=axis, ddof=ddof)
+    return np.where(sd == 0, 0, m/sd)
+
+print(signaltoo(F, axis = 0, ddof = 0))
+print(signaltoo(D, axis = 0, ddof = 0))
+plt.plot(k, S, color='blue', label="s(k)")
+plt.plot(k, D, label='s(k)+n(k)')
+plt.plot(k, F, label='s(k)+n(k)+a')
 plt.legend(loc='best')
 plt.xlabel('k')
 plt.ylabel('v')
